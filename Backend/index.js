e9a38aa18d9cc0293a10cb18b2f4cdaac5e08ee2
Nithin Kumar <sthestar1515@gmail.com>
2026-09-22 15:10:12 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const {Pool} = require('pg');
 const app = express()
+app.use(express.json());
 app.use(cors({origin: true,credentials:true}));
 const connectionString = process.env.DB_KEY
 
